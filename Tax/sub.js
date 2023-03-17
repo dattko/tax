ec22de366369d9efe827d152e96e79f0194967c2
dattko <99301568+dattko@users.noreply.github.com>
2023-03-17 10:46:25 +0900

    카카오 사인 추가

--- Tax/sub.js
+++ Tax/sub.js
@@ -76,3 +76,29 @@ back.addEventListener('click', ()=>{
    console.log('asd')
 })
 
+
+
+//셀렉트
+
+/* 셀렉트박스 보이게 하기 */
+const document.querySelector('')
+$("body").on("click", "#select", function () {
+   $("#consultation-state-ul").toggle();
+})
+
+/* 셀렉트 박스 옵션 선택 */
+$("body").on("click", "#ul li", function () {
+   var text = $(this).html();
+   $("#consultation-state-select").html(text);
+   $("#consultation-state-ul").toggle();
+})
+
+/* 셀렉트 박스 이외 선택시 보이지 않게 하기 */
+$("body").on("click", function(e){
+  if($(".select-ul").css("display") == "block"){
+      if($("#select-wrap").has(e.target).length == 0){
+          $(".select-ul").hide()
+       }
+   }
+
+})
